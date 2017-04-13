943be3bfd0258604c49dbe124560d3b5cb501b80
Ben Maakestad <ben.maakestad@daugherty.com>
2017-04-13 16:05:55 -0500

    added auto_increment to all table primary keys

--- src/main/resources/db/migration/V1__Create_users_table.sql
+++ src/main/resources/db/migration/V1__Create_users_table.sql
@@ -1,16 +1,16 @@
 create table users (
-    user_id int not null primary key,
+    user_id int not null auto_increment primary key,
     username varchar(45) not null,
     password varchar(200) not null
 );
 
 create table user_roles (
-    app_user_id int not null primary key,
+    app_user_id int not null auto_increment primary key,
     role varchar(45) not null
 );
 
 create table events (
-    event_id int not null primary key,
+    event_id int not null auto_increment primary key,
     event_name varchar(45) not null,
     event_description varchar(300) not null,
     event_date varchar(45) not null,
@@ -18,13 +18,13 @@ create table events (
 );
 
 create table pottery (
-    pottery_id int not null primary key,
+    pottery_id int not null auto_increment primary key,
     pottery_description varchar(500) not null,
     pottery_type_id int not null,
     pottery_file_name varchar(75) not null
 );
 
 create table pottery_types (
-    pottery_type_id int not null primary key,
+    pottery_type_id int not null auto_increment primary key,
     pottery_type varchar(45) not null
 );
